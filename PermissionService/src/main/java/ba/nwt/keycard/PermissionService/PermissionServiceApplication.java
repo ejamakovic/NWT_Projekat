eebdc051d7da48ea407a9af6e018b9a7852f6365
Mirza Hadžić <mhadzic10@etf.unsa.ba>
2024-03-19 03:07:59 +0100

    Added swagger 3 support

--- PermissionService/src/main/java/ba/nwt/keycard/PermissionService/PermissionServiceApplication.java
+++ PermissionService/src/main/java/ba/nwt/keycard/PermissionService/PermissionServiceApplication.java
@@ -3,10 +3,11 @@ package ba.nwt.keycard.PermissionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PermissionServiceApplication {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) {
 		SpringApplication.run(PermissionServiceApplication.class, args);
 	}
 
