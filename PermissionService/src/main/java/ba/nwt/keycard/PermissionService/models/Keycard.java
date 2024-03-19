845c07cc3527916ff5722ce79009540cfd71a8e8
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2024-03-19 02:36:19 +0100

    GenerationType.AUTO -> IDENTITY

--- PermissionService/src/main/java/ba/nwt/keycard/PermissionService/models/Keycard.java
+++ PermissionService/src/main/java/ba/nwt/keycard/PermissionService/models/Keycard.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Keycard {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
