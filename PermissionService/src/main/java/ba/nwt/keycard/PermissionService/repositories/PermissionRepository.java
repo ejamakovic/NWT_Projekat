b489eeb8571794410e3c917b780c52020b5582ec
Mirza Hadžić <mhadzic10@etf.unsa.ba>
2024-03-19 14:20:23 +0100

    Ready for task 3

--- PermissionService/src/main/java/ba/nwt/keycard/PermissionService/repositories/PermissionRepository.java
+++ PermissionService/src/main/java/ba/nwt/keycard/PermissionService/repositories/PermissionRepository.java
@@ -4,6 +4,7 @@ import ba.nwt.keycard.PermissionService.models.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+
 @Repository
 public interface PermissionRepository extends JpaRepository<Permission, Integer> {
     List<Permission> findByRoomId(Integer roomId);
