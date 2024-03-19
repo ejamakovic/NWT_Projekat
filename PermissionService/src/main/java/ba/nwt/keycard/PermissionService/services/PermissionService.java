285056fec6fd978e5b30a4b091ac2c5be0221420
Mirza Hadžić <mhadzic10@etf.unsa.ba>
2024-03-19 01:30:51 +0100

    PermissionService tweaks

--- PermissionService/src/main/java/ba/nwt/keycard/PermissionService/services/PermissionService.java
+++ PermissionService/src/main/java/ba/nwt/keycard/PermissionService/services/PermissionService.java
@@ -1,26 +1,36 @@
 package ba.nwt.keycard.PermissionService.services;
 
+import ba.nwt.keycard.PermissionService.repositories.PermissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ba.nwt.keycard.PermissionService.models.Permission;
+import ba.nwt.keycard.PermissionService.models.Keycard;
+import ba.nwt.keycard.PermissionService.models.PermissionKeycard;
 
-import java.security.Permission;
-
+@Service
 public class PermissionService {
 
-    @Autowired
-    private PermissionRepository permissionRepository;
+    private final PermissionRepository permissionRepository;
 
-    // Dodavanje nekoliko unosa u tabelu "permissions"
-    Permission permission1 = new Permission();
-        permission1.setRole(Role.ADMIN);
+    @Autowired
+    public PermissionService(PermissionRepository permissionRepository) {
+        this.permissionRepository = permissionRepository;
+    }
+
+    public void initializePermissions() {
+        // Dodavanje nekoliko unosa u tabelu "permissions"
+        ba.nwt.keycard.PermissionService.models.Permission permission1 = new ba.nwt.keycard.PermissionService.models.Permission();
+        //permission1.setRole(Role.ADMIN);
         permission1.setRoomId(1);
         permission1.setFloorId(1);
         permission1.setBuildingId(1);
         permissionRepository.save(permission1);
 
-    Permission permission2 = new Permission();
-        permission2.setRole(Role.USER);
+        ba.nwt.keycard.PermissionService.models.Permission permission2 = new ba.nwt.keycard.PermissionService.models.Permission();
+        //permission2.setRole(Role.USER);
         permission2.setRoomId(2);
         permission2.setFloorId(2);
         permission2.setBuildingId(2);
         permissionRepository.save(permission2);
+    }
 }
