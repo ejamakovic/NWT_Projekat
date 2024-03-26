df1774f9a626006eaffd2c5afbe30259c7d6a123
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2024-03-26 22:32:24 +0100

    Exception handler

---
+++ RoomService/src/main/java/ba/nwt/keycard/RoomService/controllers/ErrorHandler/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package ba.nwt.keycard.RoomService.controllers.ErrorHandler;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        Throwable cause = ex.getRootCause();
+        if (cause instanceof SQLIntegrityConstraintViolationException) {
+            SQLIntegrityConstraintViolationException sqlEx = (SQLIntegrityConstraintViolationException) cause;
+            String errorMessage = sqlEx.getMessage();
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+        return ResponseEntity.badRequest().body("Internal server error occurred.");
+    }
+}
