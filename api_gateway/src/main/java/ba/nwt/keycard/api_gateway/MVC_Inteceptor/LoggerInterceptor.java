b59987a565717876adb4a87c5c37c0cf7d8d2c3c
Dzenis Kajevic <denisgenis@yahoo.com>
2024-05-15 19:17:31 +0200

    djelimicno popravljen interceptor :)

--- api_gateway/src/main/java/ba/nwt/keycard/api_gateway/MVC_Inteceptor/LoggerInterceptor.java
+++ api_gateway/src/main/java/ba/nwt/keycard/api_gateway/MVC_Inteceptor/LoggerInterceptor.java
@@ -1,5 +1,8 @@
 package ba.nwt.keycard.api_gateway.MVC_Inteceptor;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,8 +12,15 @@ public class LoggerInterceptor implements HandlerInterceptor {
     public boolean preHandle(
             HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        System.out.println("Inside the LoggerInterceptor...");
+        try {
+            PrintStream fileOut = new PrintStream("./out.txt");
+            System.setOut(fileOut);
+            System.out.println("This goes to out.txt");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
         return true;
+
     }
 
     public void postHandle(
