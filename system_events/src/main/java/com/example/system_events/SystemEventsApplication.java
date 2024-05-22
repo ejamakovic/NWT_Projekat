9cc11a51858a3c47f56818a49d16ad22680191cf
Dzenis Kajevic <denisgenis@yahoo.com>
2024-05-22 10:17:33 +0200

    Fixed gRPC server

--- system_events/src/main/java/com/example/system_events/SystemEventsApplication.java
+++ system_events/src/main/java/com/example/system_events/SystemEventsApplication.java
@@ -1,6 +1,6 @@
 package com.example.system_events;
 
-import java.io.IOException;
+/* import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -29,6 +29,48 @@ public class SystemEventsApplication {
 	@Bean(initMethod = "start")
 	public Server grpcServer() {
 		grpcServer = ServerBuilder
+				.forPort(grpcPort)
+				.addService(new SystemEventsServerImplementation())
+				.build();
+
+		// Start the server and block until it is shut down
+		try {
+			grpcServer.start();
+			grpcServer.awaitTermination();
+		} catch (IOException | InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
+		return grpcServer;
+	}
+} */
+
+import io.grpc.Server;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.system_events.SystemEventsServer.SystemEventsServerImplementation;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class SystemEventsApplication {
+
+	@Value("${grpc.server.port}")
+	private int grpcPort;
+
+	private Server grpcServer;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemEventsApplication.class, args);
+	}
+
+	@Bean(initMethod = "start")
+	public Server grpcServer() {
+		grpcServer = NettyServerBuilder
 				.forPort(grpcPort)
 				.addService(new SystemEventsServerImplementation())
 				.build();
